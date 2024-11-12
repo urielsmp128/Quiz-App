bd38b28a2cf9b1e66ef9e5b0577986f3c1184549
urielsmp128 <urielsmp128@outlook.com>
2024-11-11 22:46:23 -0600

    Creating basic components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Quiz from './components/Quiz';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Quiz />
   </React.StrictMode>
 );
 
